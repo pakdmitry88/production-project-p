367901aa3dec682b7ec7fcef5636ac4b6aa019b5
pakds <setevieru@yandex.ru>
2023-02-18 01:47:45 +0900

    fix test

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,18 +1,19 @@
 /* eslint-disable arrow-body-style */
 import { fireEvent, render, screen } from '@testing-library/react';
 import { withTranslation } from 'react-i18next';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
 // eslint-disable-next-line max-len
 import { renderWithTranslation } from '../../../../shared/lib/tests/renderWithTranslation/renderWithTranslation';
 
 describe('Sidebar', () => {
     test('Test render', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 
     test('Test toggle', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn);
